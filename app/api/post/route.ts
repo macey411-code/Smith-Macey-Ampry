7938f7592d441108b39152e4830aa5aac451f918
macey411-code <macey411@gmail.com>
2024-11-05 04:43:37 +0000

    API and data submit

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -1,5 +1,28 @@
 export async function POST(request: Request) {
-  return new Response("POST request");
+  // Get data!
+  const formData = await request.json();
+
+  const { name, email, comments } = formData;  // Extract Name, Email, and Comments
+
+  // Slack Webhook 
+  const slackWebhookUrl = "https://hooks.slack.com/services/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";
+
+  // Create the payload to send to Slack
+  const payload = {
+    text: `New submission: \nName: ${name}\nEmail: ${email}\nComments: ${comments}`,
+  };
+
+  // Send the data to Slack
+  await fetch(slackWebhookUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
+
+  // Respond back to the client
+  return new Response("Form submitted successfully!");
 }
 
 export async function GET(request: Request) {
